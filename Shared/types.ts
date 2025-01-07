b6502a98dd278ec55bff6e2c7c7ea90e41adf147
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-01-07 18:42:50 +0300

    Add files via upload

--- Shared/types.ts
+++ Shared/types.ts
@@ -1,18 +1,38 @@
-import { RowDataPacket } from "mysql2/index";
-
-
-export interface IComment{
-    id: string;
-    name: string;
-    email: string;
-    body: string;
-    productId: string;
+export interface IComment {
+  id: string;
+  name: string;
+  email: string;
+  body: string;
+  productId: string;
 }
 
 export interface IProduct {
-    id: string;
-    title: string;
-    description: string;
-    price: number;
-    comments?: IComment[];
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  thumbnail?: IProductImage;
+  comments?: IComment[];
+  images?: IProductImage[];
 }
+
+export interface IProductImage {
+  id: string;
+  productId: string;
+  main: boolean;
+  url: string;
+}
+
+export interface IProductFilterPayload {
+  title?: string;
+  description?: string;
+  priceFrom?: number;
+  priceTo?: number;
+}
+
+export interface IAuthRequisites {
+  username: string;
+  password: string;
+}
+
+export type SimilarProduct = Pick<IProduct, "id" | "title" | "price">;
