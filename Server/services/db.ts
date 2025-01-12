abc079b81a11dcacdf89eb681400d34ced36369a
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-01-12 23:45:55 +0300

    Add files via upload

--- Server/services/db.ts
+++ Server/services/db.ts
@@ -1,21 +1,24 @@
-import mysql, { Connection } from "mysql2/promise";
+import { createConnection, Connection } from "mysql2/promise";
+
+const { DB_NAME } = process.env;
 
 export async function initDataBase(): Promise<Connection | null> {
-    let connection: Connection | null = null;
-  
-    try {
-        connection = await mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            password: '29aiVBpTG',
-            user: "editor",
-            database: "ProductsApplication",
-        });
-    } catch (e) {
-        console.error(e.message || e);
-        return null;
-    }
-  
-    console.log(`Connection to DB ProductsApplication established`);
-    return connection;
+  let connection: Connection | null = null;
+
+  try {
+    connection = await createConnection({
+      host: "localhost",
+      port: 3306,
+      password: '29aiVBpTG',
+      user: "editor",
+      database: "ProductsApplication",
+    });
+  } catch (e: any) {
+    console.error(e.message || e);
+    return null;
+  }
+
+  console.log(`Connection to DB ${DB_NAME} established`);
+
+  return connection;
 }
--- Server/services/server.ts
+++ Server/services/server.ts
@@ -1,18 +1,17 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
 
-
-const host = process.env.LOCAL_HOST;
+const host = process.env.LOCAL_PATH;
 const port = Number(process.env.LOCAL_PORT);
 
-export function initServer(): Express{
-    const app = express();
+export function initServer(): Express {
+  const app = express();
 
-    const jsonMiddleware = express.json();
-    app.use(jsonMiddleware);
+  const jsonMiddleware = express.json();
+  app.use(jsonMiddleware);
 
-    app.listen(port, host, ()=>{
-        console.log(`Server running on port ${port}`);
-    });
+  app.listen(port, host, () => {
+    console.log(`Server running on port ${port}`);
+  });
 
-    return app;
+  return app;
 }
