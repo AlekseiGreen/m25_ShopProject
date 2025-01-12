a6d9c339221b27106cdc338e999c19896a36a9b6
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-01-12 23:43:44 +0300

    Add files via upload

--- Shop.Admin/index.ts
+++ Shop.Admin/index.ts
@@ -1,32 +1,37 @@
-import express, { Express } from "express";
-import { productsRouter } from "./controllers/products.controller" 
-import { authRouter } from "./controllers/auth.controller" 
+import express, { Express, NextFunction, Request, Response } from "express";
+import { productsRouter } from "./controllers/products.controller";
 import layouts from "express-ejs-layouts";
 import bodyParser from "body-parser";
+import { authRouter, validateSession } from "./controllers/auth.controller";
 import session from "express-session";
 
-
 export default function (): Express {
-    const app = express();
+  const app = express();
 
-    app.use(session({
-        secret: "abcde",
-        saveUninitialized: false,
-        resave: false
-    }));
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false
+  }));
 
-    app.use(express.json());
-    app.use(bodyParser.urlencoded({ extended: false })); //чтобы обрабатывать данные формы x-www-form-urlencoded
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.set("view engine", "ejs");
-    app.set("views", "Shop.Admin/views");
+  app.set("view engine", "ejs");
+  app.set("views", "Shop.Admin/views");
+  app.use(layouts);
 
-    app.use(layouts);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.location = req.headers.host + req.originalUrl;
+    next();
+  });
 
-    app.use(express.static(__dirname + "/public"));
+  app.use(express.static(__dirname + "/public"));
 
-    app.use("/auth", authRouter);
-    app.use("/", productsRouter);
+  app.use(validateSession);
 
-    return app;
+  app.use("/auth", authRouter);
+  app.use("/", productsRouter);
+
+  return app;
 }
